2e688676ad37933aa9b0a1453aa6d6976eca5550
bastienwcs <email>
2019-07-19 11:19:48 +0200

    fix

--- app/src/main/java/fr/wildcodeschool/sendemail/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/sendemail/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMail(User user, final Consumer<Boolean> listener) {
 
-        // TODO : remove this comment from release
+        // TODO : remove this comment for release
         /*if (BuildConfig.DEBUG) {
             // if the application is in debug variant, no email is sent
             return;
